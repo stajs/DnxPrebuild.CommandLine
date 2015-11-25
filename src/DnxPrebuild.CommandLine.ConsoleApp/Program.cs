24d27e46a51e8f73aca17c05f3fe1750cca9d097
stajs <shannonsnz@gmail.com>
2015-11-25 21:58:20 +1300

    Write to a log file.

---
+++ src/DnxPrebuild.CommandLine.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace DnxPrebuild.CommandLine.ConsoleApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var filePath = Path.Combine(Environment.CurrentDirectory, "dnx-prebuild.log");
+			File.AppendAllText(filePath, DateTime.Now.ToLongTimeString() + Environment.NewLine);
+		}
+	}
+}
---
+++ src/DnxPrebuild.CommandLine.ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DnxPrebuild.CommandLine.ConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DnxPrebuild.CommandLine.ConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("edb3d8ec-a158-4259-a62e-1e5641785154")]
