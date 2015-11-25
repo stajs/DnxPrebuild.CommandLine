dcecc9e09a16e2380c650e53f7aa4ec5d78642c4
stajs <shannonsnz@gmail.com>
2015-11-25 22:15:55 +1300

    WriteLine().

--- src/DnxPrebuild.Library.ConsoleApp/Program.cs
+++ src/DnxPrebuild.Library.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace DnxPrebuild.Library.ConsoleApp
 	{
 		public static void Main(string[] args)
 		{
+			Console.WriteLine("DnxPrebuild.Library.ConsoleApp");
 			var filePath = Path.Combine(Environment.CurrentDirectory, "dnx-prebuild.log");
 			File.AppendAllText(filePath, DateTime.Now.ToLongTimeString() + Environment.NewLine);
 		}
